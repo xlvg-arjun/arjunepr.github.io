a5e477aa18d73aca2ec9b88dd565dd0d78fff974
Arjun Raju Pillai <revarjunr@gmail.com>
2017-06-08 02:17:27 +0530

    Finish most of designing.

--- brunch-config.js
+++ brunch-config.js
@@ -1,4 +1,21 @@
+exports.awaitWriteFinish = true;
+
 exports.files = {
-	javascripts: {joinTo: 'app.js'},
+	javascripts: {
+		joinTo: {
+			'vendor.js': /^(?!app)/
+		},
+	},
 	stylesheets: {joinTo: 'style.css'}
+};
+
+
+exports.plugins = {
+	autoReload: {
+		enabled: {
+			css: true,
+			js: true,
+			assets: true
+		}
+	}
 };
